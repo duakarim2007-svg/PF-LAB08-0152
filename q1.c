e9d8f24f3b3f659f58501e05d9ffee382f7bed64
duakarim2007-svg <duakarim2007@gmail.com>
2025-10-23 09:28:58 +0500

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int i , j;
+	printf("    Multiplication table (1-5)\n");
+	for (i=1 ; i<=5 ; i++){
+		printf("%d: ",i);
+		for (j=1 ; j<=5  ; j++){
+			printf("%4d", j*i);
+			
+			}
+		printf("\n");	
+		}
+		
+	
+}
